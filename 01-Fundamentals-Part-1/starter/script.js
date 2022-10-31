111ed88552b4e7511736cc15e39844e02564ea55
InsolentPaul <pavlo.kravchenko@proton.me>
2022-10-31 11:59:07 +0200

    first commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,134 @@
+"use strict";
+/* let js = "amaizing";
+console.log(40 + 10 + 12 - 3);
+
+let firstName = "Pete";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName); 
+
+let test;
+console.log(test);
+console.log(typeof test);
+test = "changed variable";
+console.log(test);
+console.log(typeof test);
+
+console.log(typeof null);
+
+// variables
+const now = 2038
+const ageJulia = now - 1991;
+const agePaul = now - 1989;
+console.log(ageJulia, agePaul);
+console.log(ageJulia * 2, agePaul / 2, 2 ** 3);
+
+const firstName = "Pete";
+const lastName = "Kravchenko";
+console.log(firstName + " " + lastName);
+
+// assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+console.log(x);
+
+// comparison operators
+console.log(ageJulia > agePaul);
+console.log(ageJulia >= 20);
+
+const now = 2038;
+const ageJulia = now - 1991;
+const agePaul = now - 1989;
+
+console.log(now - 1991 < now - 1989);
+
+let x, y;
+x = y = 25 - 10 - 5; // = 10
+console.log(x, y);
+
+const averageAge = (ageJulia + agePaul) / 2;
+console.log(ageJulia, agePaul);
+console.log(averageAge); */
+
+// CHALLENGE Time
+
+/* 
+Test Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+Test Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall. */
+
+/* const massMark = 78; // kilos
+const heightMark = 1.69; // meters
+const massJohn = 92; // kilos
+const heightJohn = 1.95; // meters
+
+const markBMI = massMark / heightMark ** 2;
+const johnBMI = massJohn / heightJohn ** 2;
+const markHigherBMI = markBMI > johnBMI;
+
+console.log(markBMI);
+console.log(johnBMI);
+console.log(markHigherBMI);
+
+// book practice
+console.log(`sum = ${1 + 2}`);
+console.log("sum = " + (1 + 2));
+
+let value = true;
+value = String(value); 
+
+// alert(typeof value);
+console.log("" + 1 + 0);
+console.log("" - 1 + 0);
+console.log(true + false);
+console.log(6 / "3");
+console.log("2" * "3");
+console.log(4 + 5 + "px");
+console.log(7 / 0);
+console.log(" -9 " + 5);
+console.log(" -9 " - 5);
+console.log(null + 1);
+console.log(undefined + 1);
+console.log(" \t \n" - 2);
+let a = 3;
+let b = 8;
+console.log(b ** (1 / 2)); 
+
+const workTime = prompt("what time is now?");
+if (workTime < 9 || workTime > 18) {
+  console.log("office closed");
+} else {
+  console.log("office open");
+}
+
+let tempUserName = 0;
+const defaultUserName = "Pete";
+const userName = tempUserName || defaultUserName || "no name";
+console.log(userName);
+
+let x = false || 3;
+console.log(x);
+
+const age = 98;
+if (!(age >= 14 && age <= 90)) {
+  console.log("OK!");
+} else {
+  console.log("DENIED");
+}
+
+const userLogin = prompt("Type your Username:", "");
+if (userLogin == "admin") {
+  const userPassword = prompt("Type your Password:", "");
+  if (userPassword == "itsme") {
+    alert("Hello Sir!");
+  } else if (userPassword == "" || userPassword == null) {
+    alert("CANCELED!");
+  } else {
+    alert("ERROR!");
+  }
+} else if (userLogin == "" || userLogin == null) {
+  alert("CANCELED!");
+} else {
+  alert("ERROR!");
+} */
+
+console.log("check one two");
